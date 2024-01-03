1ebe0b31d94b2e64c18851603fd6c154b66c1533
Grizzly21f <taras.firhuk72@gmail.com>
2024-01-04 00:42:14 +0200

    Node

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-const {help} =require('./helper');
+// const path = require('path');
+// const fs = require('fs');
+// //
+//  const file = path.join(__dirname,'Main','Dir5','text5.txt');
+//
+// // fs.writeFile(file, 'hello', (err)=>{
+// //   if (err) throw new Error();
+// // })
+//
+// // fs.mkdir(path.join(__dirname,'Main','Dir5'),(err)=>{
+// //   if (err) throw new Error()
+// // })
 
-help();
-
-
-const os = require('os');
-
-console.log(os.cpus())
+// const stats = fs.statSync(file);
+//
+//    if (stats.isFile()) {
+//        console.log('File');
+//    } else if (stats.isDirectory()) {
+//        console.log('Dir');
+//    }
+//
