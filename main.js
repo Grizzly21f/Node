8121c80e3dc01871bdf5ddb11aece18ddaade99f
Grizzly21f <taras.firhuk72@gmail.com>
2024-01-04 16:00:52 +0200

    Node

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
-// const path = require('path');
-// const fs = require('fs');
-// //
-//  const file = path.join(__dirname,'Main','Dir5','text5.txt');
-//
-// // fs.writeFile(file, 'hello', (err)=>{
-// //   if (err) throw new Error();
-// // })
-//
-// // fs.mkdir(path.join(__dirname,'Main','Dir5'),(err)=>{
-// //   if (err) throw new Error()
-// // })
+const fs = require('fs').promises;
+const path = require('path');
 
-// const stats = fs.statSync(file);
-//
-//    if (stats.isFile()) {
-//        console.log('File');
-//    } else if (stats.isDirectory()) {
-//        console.log('Dir');
-//    }
-//
+(async () => {
+        await fs.mkdir(path.join(__dirname, 'Main'));
+
+        for (let i = 1; i <= 5; i++) {
+            await fs.mkdir(path.join(__dirname, 'Main', `Dir${i}`));
+            await fs.writeFile(path.join(__dirname, 'Main', `file${i}`), `hello${i}`);
+        }
+
+        const directoryPath = path.join(__dirname, 'Main');
+        const files = await fs.readdir(directoryPath);
+
+        for (const file of files) {
+            const filePath = path.join(directoryPath, file);
+            const stats = await fs.stat(filePath);
+
+            if (stats.isFile()) {
+                console.log(`${file} - file`);
+            } else if (stats.isDirectory()) {
+                console.log(`${file} - Dir`);
+            }
+        }
+})();
