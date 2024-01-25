42435ab48f4c7a0c4ce3bfc7dc288c8765909538
Grizzly21f <taras.firhuk72@gmail.com>
2024-01-25 16:52:33 +0200

    Node HW7 + MONGO DATA BASE + AUTHORIZATION + EMAIL + JOI

---
+++ src/validators/user.validator.ts
@@ -0,0 +1,39 @@
+import joi from "joi";
+
+import { regexConstant } from "../constants/regex.constant";
+
+export class UserValidator {
+    private static password = joi.string().regex(regexConstant.PASSWORD).trim();
+    private static email = joi
+        .string()
+        .regex(regexConstant.EMAIL)
+        .lowercase()
+        .trim();
+    private static userName = joi.string().min(3).max(50).trim().messages({
+        "string.empty": "{{#label}} not be empty",
+        "string.max":
+            "{{#label}} length must be less than or equal to {{#limit}} characters long2",
+        "string.min":
+            "{{#label}} length must be at least {{#limit}} characters long2",
+
+
+    });
+    private static age = joi.number().min(18).max(100).integer();
+
+    public static create = joi.object({
+        email: this.email.required(),
+        password: this.password.required(),
+        name: this.userName.required(),
+        age: this.age.required(),
+    });
+
+    public static update = joi.object({
+        name: this.userName,
+        age: this.age,
+    });
+
+    public static login = joi.object({
+        email: this.email.required(),
+        password: this.password.required(),
+    });
+}
